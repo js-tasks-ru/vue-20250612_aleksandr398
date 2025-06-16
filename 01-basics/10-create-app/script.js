e27b0047ff821638dabf16b517257bc5ae3fd6db
ATeryohin <alex_teryohin@mail.ru>
2025-06-17 00:15:19 +0500

    Solve 01-basics 10-createApp

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,32 @@
-import { defineComponent, createApp } from 'vue'
+import { defineComponent, createApp } from 'vue/dist/vue.esm-browser.js'
+
+const App = defineComponent({
+    name: "App",
+
+    setup() {
+        function getLocalDate() {
+            return new Date().toLocaleString(navigator.language, {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                }
+            )
+        };
+
+        return {
+            getLocalDate,
+        }
+    },
+
+    template: `
+        <div>
+            <a> Сегодня {{ getLocalDate() }}</a>
+        </div>
+    `,
+})
+
+const app = createApp(App)
+
+const vm = app.mount('#app')
+
+window.vm = vm
